cc069e9a4bf9bf248be882b12f4ca18eff4278ff
Raul-Holik <113007938+Raul-Holik@users.noreply.github.com>
2023-01-25 09:20:25 +0100

    Add files via upload

---
+++ Ispit/Ispit/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ispit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.
+            //Napišite program u kojem ćete zatražiti od korisnika da unese niz znakova te će svaki razmak zamijeniti znakom '_'.
+
+            string znak;
+
+            Console.WriteLine("Upiši niz znakova sa razmacima. ");
+            znak =(Console.ReadLine());
+
+            Console.WriteLine(znak.Replace(" ", "_"));
+
+            Console.ReadKey();
+
+        }
+    }
+}
